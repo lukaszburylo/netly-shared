77cd55886773b03d667d8722c8500685e3e1c2ea
Lukasz Burylo <lukasz@burylo.com>
2025-11-21 17:20:46 +0100

    Expose ServiceResult and ServiceMetadata

--- netly_shared/__init__.py
+++ netly_shared/__init__.py
@@ -1,3 +1,4 @@
 from .ResponseTemplate import ResponseTemplate
+from .response_schema import ServiceResult, ServiceMetadata
 
-__all__ = ["ResponseTemplate"]
+__all__ = ["ResponseTemplate", "ServiceResult", "ServiceMetadata"]
